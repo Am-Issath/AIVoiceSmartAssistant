3afdadc9ac02e43f0c29da181487f4238e8565aa
Issath <74565253+Am-Issath@users.noreply.github.com>
2023-12-15 22:49:32 +0530

    Update timer_and_youtube.py
    Used pywhtt library then if you search a song it will directly play with the default web browser

--- ArjunReddy AI Smart Virtual Assistant/timer_and_youtube.py
+++ ArjunReddy AI Smart Virtual Assistant/timer_and_youtube.py
@@ -1,18 +1,46 @@
-# timer_and_youtube_functions.py
-
 import time
 import webbrowser
+import pywhatkit
+from listen_speak import speak
 
+# Function to set a timer
 def set_timer(minutes):
     seconds = minutes * 60
-    time.sleep(seconds)
-    print("Timer expired. Time's up!")
+    end_time = time.time() + seconds
 
+    speak(f"Timer set for {minutes} minutes.")
+
+    while time.time() < end_time:
+        remaining_time = int(end_time - time.time())
+        if remaining_time % 60 == 0:
+            speak(f"{remaining_time // 60} minutes remaining.")
+        time.sleep(1)
+
+    speak("Timer expired. Time's up!")
+
+# Modify the set_timer function to store the end_time globally
+end_time = 0  # Initialize end_time globally
+
+# Add a simplified check_timer_status function
+def check_timer_status():
+    remaining_time = int(end_time - time.time())
+    speak(f"The timer has {remaining_time // 60} minutes and {remaining_time % 60} seconds remaining.")
+
+
+# def play_youtube(query):
+#     url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
+#     try:
+#         webbrowser.open(url, new=2)
+#         print(f"Now playing {query} on YouTube.")
+#     except Exception as e:
+#         print(f"Error playing YouTube video: {e}")
+#         print("Sorry, there was an error playing the song on YouTube.")
+
+# Function to play a song on YouTube
 def play_youtube(query):
-    url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
     try:
-        webbrowser.open(url, new=2)
-        print(f"Now playing {query} on YouTube.")
+        pywhatkit.playonyt(query)
+        speak(f"Now playing {query} on YouTube.")
     except Exception as e:
         print(f"Error playing YouTube video: {e}")
-        print("Sorry, there was an error playing the song on YouTube.")
+        speak("Sorry, there was an error playing the song on YouTube.")
